d146271279b3d2cdbdb9c02186902ef76d677b16
pedrobdias <pedrobungenstabdias@gmail.com>
2024-08-13 18:26:32 -0300

    python
    python

---
+++ pythonPTBR.py
@@ -0,0 +1,23 @@
+def printar():
+    print('Olá, Mundo!')
+
+
+def se():
+    num = 10
+    if num > NumEscrSe:
+        print('True')
+    else:
+        print('False')
+
+
+def escrever():
+    example = input('escreva algo: ')
+
+
+def NumEscrSe():
+    print('a função se da um if de se o valor for maior do que 10 vai dar True mas senão vai dar False')
+    num = int(input('digite um numero: '))
+    if num > 10:
+        print('True')
+    else:
+        print('False')
